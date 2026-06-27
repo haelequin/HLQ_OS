f3dcad1e11560c8b138bc38c30484c75a18c892b
DDKhoa2k8 <ddkmgb999@gmail.com>
2026-06-27 21:18:44 +0700

    Add basic text printing library (VGA_writer.rs)

--- src/lib.rs
+++ src/lib.rs
@@ -10,34 +10,38 @@ pub mod vga_writer;
 #[unsafe(no_mangle)]
 pub extern "C" fn rust_main() -> ! {
     // Pointer to the VGA text buffer 
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    // Write "OK" into the top-left corner of the screen
-    unsafe {
-        *vga_buffer.offset(320) = b'R';
-        *vga_buffer.offset(321) = 0x9f;
-        *vga_buffer.offset(322) = b'U';
-        *vga_buffer.offset(323) = 0x9f;        
-        *vga_buffer.offset(324) = b'S';
-        *vga_buffer.offset(325) = 0x9f;        
-        *vga_buffer.offset(326) = b'T';
-        *vga_buffer.offset(327) = 0x9f;
+    // unsafe {
+        // *vga_buffer.offset(320) = b'R';
+        // *vga_buffer.offset(321) = 0x9f;
+        // *vga_buffer.offset(322) = b'U';
+        // *vga_buffer.offset(323) = 0x9f;        
+        // *vga_buffer.offset(324) = b'S';
+        // *vga_buffer.offset(325) = 0x9f;        
+        // *vga_buffer.offset(326) = b'T';
+        // *vga_buffer.offset(327) = 0x9f;
 
-        *vga_buffer.offset(480) = b'H';
-        *vga_buffer.offset(481) = 0x9f;
-        *vga_buffer.offset(482) = b'A';
-        *vga_buffer.offset(483) = 0x9f;        
-        *vga_buffer.offset(484) = b'I';
-        *vga_buffer.offset(485) = 0x9f;        
-        *vga_buffer.offset(486) = b' ';
-        *vga_buffer.offset(487) = 0x9f;
-        *vga_buffer.offset(488) = b'G';
-        *vga_buffer.offset(489) = 0x9f;
-        *vga_buffer.offset(490) = b'A';
-        *vga_buffer.offset(491) = 0x9f;
-        *vga_buffer.offset(492) = b'Y';
-        *vga_buffer.offset(493) = 0x9f;
-    }
+        // *vga_buffer.offset(480) = b'H';
+        // *vga_buffer.offset(481) = 0x9f;
+        // *vga_buffer.offset(482) = b'A';
+        // *vga_buffer.offset(483) = 0x9f;        
+        // *vga_buffer.offset(484) = b'I';
+        // *vga_buffer.offset(485) = 0x9f;        
+        // *vga_buffer.offset(486) = b' ';
+        // *vga_buffer.offset(487) = 0x9f;
+        // *vga_buffer.offset(488) = b'G';
+        // *vga_buffer.offset(489) = 0x9f;
+        // *vga_buffer.offset(490) = b'A';
+        // *vga_buffer.offset(491) = 0x9f;
+        // *vga_buffer.offset(492) = b'Y';
+        // *vga_buffer.offset(493) = 0x9f;
+    // }
+
+    let mut vga_writer = vga_writer::VGAWriter::init();
+
+    vga_writer.line_o = 2;
+    vga_writer.print("Hello world!");
 
     loop {}
 }
--- src/vga_writer.rs
+++ src/vga_writer.rs
@@ -1,41 +1,126 @@
-const COL_SIZE:u8 = 80;
-const ROW_SIZE:u8 = 25;
+const COL_SIZE:isize = 80;
+const ROW_SIZE:isize = 25;
 
 pub struct VGAWriter {
     pub vga_addr: *mut u8,
-    pub line_char_o: u8,//number of char per line
-    pub line_o: u8,//number of line
+    pub line_char_o: isize,//index of currect column
+    pub line_o: isize,//index of current row (line)
     pub color: u8,
 }
 
 impl VGAWriter {
     pub fn init() -> VGAWriter {
         VGAWriter {
-            vga_addr: 0x8000 as *mut u8,
+            vga_addr: 0xb8000 as *mut u8,
             line_char_o: 0,
             line_o: 0,
-            color: 0x002f,//Light green
+            color: 0x2f,//Light green
         }
     }
 
+    ///### Set text color
+    /// 
+    ///### 8 bit input
+    /// 
+    /// first 4 bit define text color (| Bright | Red | Green | Blue |)
+    /// 
+    /// second 4 bit define background color (| Bright | Red | Green | Blue |)
+    /// 
+    /// ### Example:
+    /// ```no_run
+    /// pub mod vga_writer;
+    /// 
+    /// let mut vga = vga_writer::VGAWriter::init();
+    /// 
+    /// //0x2f -> 0010(second 4 is green) 1111(first 4 bit mean white)
+    /// vga.set_color(0x2f);//Set background color green, white text
+    /// ```
     pub fn set_color(&mut self, color:u8) {
         self.color = color;
     }
 
-    pub fn print_char(&mut self, c:u8) {
-        self.line_char_o += 1;
-
-        let offset = COL_SIZE * self.line_o * 2 + self.line_char_o;
-
-        unsafe {
-            *self.vga_addr.offset(offset.into()) = c;
-            *self.vga_addr.offset((offset + 1).into()) = self.color;
-        }
+    /// ### Break line
+    /// 
+    /// ### Example:
+    /// ```no_run
+    /// pub mod vga_writer;
+    /// 
+    /// let mut vga = vga_writer::VGAWriter::init();
+    /// 
+    /// vga.print_char("A");
+    /// 
+    /// vga.new_line();
+    /// 
+    /// vga.print_char("A");
+    /// 
+    /// //Expected output:
+    /// //A
+    /// //A
+    /// ```
+    pub fn new_line(&mut self) {
+        self.line_o += 1;//add new line
     }
 
+    /// ### Print a single character
+    /// 
+    /// ### Example:
+    /// ```no_run
+    /// pub mod vga_writer;
+    /// 
+    /// let mut vga = vga_writer::VGAWriter::init();
+    /// 
+    /// vga.print_char("A");
+    /// //Expected output:
+    /// //A
+    /// ```
+    pub fn print_char(&mut self, c:u8) {
+        let offset = (COL_SIZE * self.line_o + self.line_char_o) * 2;
+        
+        unsafe {
+            *self.vga_addr.offset(offset) = c;
+            *self.vga_addr.offset(offset + 1) = self.color;
+        }
+
+        self.line_char_o += 1;
+    }
+
+    /// ### Print a complete string
+    /// 
+    /// ### Example:
+    /// ```no_run
+    /// pub mod vga_writer;
+    /// 
+    /// let mut vga = vga_writer::VGAWriter::init();
+    /// 
+    /// vga.print("Hello World");
+    /// //Expected output:
+    /// //Hello World
+    /// ```
     pub fn print(&mut self, content: &str) {
         for s in content.bytes() {
             self.print_char(s);
         }
+    }
+
+        /// ### Print a complete string
+    /// 
+    /// ### Example:
+    /// ```no_run
+    /// pub mod vga_writer;
+    /// 
+    /// let mut vga = vga_writer::VGAWriter::init();
+    /// 
+    /// vga.println("Hello");
+    /// vga.println("World");
+    /// //Expected output:
+    /// //Hello 
+    /// //World
+    /// ```
+    pub fn println(&mut self, content: &str) {
+        for s in content.bytes() {
+            self.print_char(s);
+        }
+
+        self.new_line();
     }
 }
